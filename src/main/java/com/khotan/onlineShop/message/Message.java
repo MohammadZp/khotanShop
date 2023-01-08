673a184c747b4263d0e68bf97ba5791bd21da4d2
MojganZahedi <122098660+MojganZahedi@users.noreply.github.com>
2023-01-08 11:14:40 -0800

    hello

--- src/main/java/com/khotan/onlineShop/message/Message.java
+++ src/main/java/com/khotan/onlineShop/message/Message.java
@@ -3,4 +3,5 @@ package com.khotan.onlineShop.message;
 public class Message {
     public static final String A="";
     public static final String CUSTOMER_USER = "مشتری";
+    public static final String ADD_USER_SUCCESSFUL = "کاربر با موفقیت افزوده شد.";
 }
